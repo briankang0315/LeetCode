9034d579bbcf9a7bab919e0a8db18f646a783ac2
Brian <64062126+briankang0315@users.noreply.github.com>
2025-06-25 15:13:08 +1000

    Time: 5 ms (49.19%) | Memory: 57.7 MB (22.03%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,13 +1,15 @@
 function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number, number>();
 
-    for (let i = 0; i < nums.length; i++){
-            for (let j = i+1; j < nums.length; j++){
-                let k = nums[i] + nums[j];
-                
-                if (k == target){
-                    const x: number[] = [i, j];
-                    return x
-                }
-            }
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement)!, i];
         }
-};
+
+        map.set(nums[i], i);
+    }
+
+    return []; 
+}
