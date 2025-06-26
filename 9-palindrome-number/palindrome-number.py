17267239d90c0868a5dadeecae4910758c17d088
Brian <64062126+briankang0315@users.noreply.github.com>
2025-06-26 20:01:59 +1000

    Time: 3 ms (88.52%) | Memory: 17.9 MB (39.58%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        arr = list(str(x))
+        leng = len(arr)
+        limit = math.ceil(leng/2)
+        a = 0
+        j = -1
+        for i in range(limit):
+            if arr[i] != arr[j]:
+                return False
+            elif arr[i] == arr[j]: 
+                a+=1
+                j-=1
+        
+
+        if a == limit:
+            return True
+
+            
+        
+ 
